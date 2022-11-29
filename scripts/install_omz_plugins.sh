1606cb84ac8e67cd6c3cf5c36a7aad5b497b4ae8
ephoris <huynh.n.andy@gmail.com>
2022-11-29 01:01:26 -0500

    Add zsh plugin scripts

---
+++ scripts/install_omz_plugins.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
