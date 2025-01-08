5fa43de31ab28ce6f294b7c08ecaf0312f3ee205
ephoris <huynh.n.andy@gmail.com>
2025-01-08 11:38:26 -0500

    [Neovim] test out copilot

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -106,4 +106,27 @@ return {
             require('ufo').setup()
         end,
     },
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = false,
+                    hide_during_completion = true,
+                    debounce = 75,
+                    keymap = {
+                        accept = "<M-l>",
+                        accept_word = false,
+                        accept_line = false,
+                        next = "<M-]>",
+                        prev = "<M-[>",
+                        dismiss = "<C-]>",
+                    },
+                },
+            })
+        end,
+    },
 }
